4ac4d1dda42d5a92d6bcda96037b62edbaa0359f
cyberphor <victor.fernandeziii@gmail.com>
2026-09-03 20:20:31 -0400

    Set dynamic default for SECRET_KEY

--- backend/kaiju/settings.py
+++ backend/kaiju/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from os import environ, getenv
 from pathlib import Path
+from secrets import token_urlsafe
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = environ["SECRET_KEY"]
+SECRET_KEY = environ.get("SECRET_KEY") or token_urlsafe(50)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
