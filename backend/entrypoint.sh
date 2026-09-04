ba3b3aed0ece09d8530aab71d5fa52862e3e7d19
cyberphor <victor.fernandeziii@gmail.com>
2026-09-03 22:37:52 -0400

    Update Dockerfile

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,6 +2,5 @@
 
 set -e
 
-python manage.py migrate
-
-uvicorn kaiju.asgi:application --host 0.0.0.0 --port 8000
+uv run manage.py migrate
+uv run uvicorn kaiju.asgi:application --host 0.0.0.0 --port 8000
